5ca92174424644bade2bd337aaeffe2a51601bdb
CleanDev1 <prodevdevdev@gmail.com>
2025-06-27 12:11:06 -0800

    Create chat.js

---
+++ api/chat.js
@@ -0,0 +1,30 @@
+const responses = require('../responses.json');
+
+module.exports = async (req, res) => {
+  try {
+    const { message } = req.body;
+    
+    let response;
+    if (/hello|hi|hey/i.test(message)) {
+      response = responses.greetings[Math.floor(Math.random() * responses.greetings.length)];
+    } 
+    else if (/\?$/.test(message)) {
+      response = responses.questions[Math.floor(Math.random() * responses.questions.length)];
+    }
+    else if (/nice|good|great|awesome/i.test(message)) {
+      response = responses.compliments[Math.floor(Math.random() * responses.compliments.length)];
+    }
+    else {
+      response = responses.default[Math.floor(Math.random() * responses.default.length)];
+    }
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json({
+      response: response,
+      model: "github-ai-v1",
+      timestamp: new Date().toISOString()
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
